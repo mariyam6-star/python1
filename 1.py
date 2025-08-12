4988cbd611975f3a3f934799c77fc72529ae7880
mariyam y sidat <mariyamsidat8899@gmail.com>
2025-08-12 10:20:24 +0530

    string functions

---
+++ 1.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+sum=n1+n2
+print("sum of two number is :",sum)
+s=str(input("Enter any string :"))
+print("convert in upparcase :",s.upper()) 
